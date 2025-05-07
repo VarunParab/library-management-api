03cbd839552aafb0a51950461d1ea3f4c3ecfcd8
VarunParab <90065369+VarunParab@users.noreply.github.com>
2025-05-07 22:58:22 +0530

    made changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { BooksModule } from './books/books.module';
+import { LoggerMiddleware } from './/middleware/logger.middleware';
+import { RateLimiterModule } from 'nestjs-rate-limiter';
 
 @Module({
   imports: [
@@ -15,9 +17,17 @@ import { BooksModule } from './books/books.module';
         uri: configService.get<string>('MONGODB_URI'),
       }),
     }),
+    RateLimiterModule.register({
+      points: 10, // Max 10 requests
+      duration: 60, // per 60 seconds per IP
+    }),
     BooksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -2,35 +2,44 @@ import { Controller, Get, Post, Body, Param, Delete, Put, Query } from '@nestjs/
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { RateLimit } from 'nestjs-rate-limiter';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Post()
+  @RateLimit({ points: 5, duration: 60 }) // 5 POST requests/min per IP
   create(@Body() createBookDto: CreateBookDto) {
     return this.booksService.create(createBookDto);
   }
 
   @Get()
+  @RateLimit({ points: 10, duration: 60 }) // 10 GETs/min
   findAll(@Query('page') page = 1, @Query('limit') limit = 10) {
     return this.booksService.findAll(+page, +limit);
   }
+
   @Get('/search')
+  @RateLimit({ points: 10, duration: 60 })
   search(@Query('query') query: string) {
     return this.booksService.search(query);
   }
+
   @Get(':id')
+  @RateLimit({ points: 10, duration: 60 })
   findOne(@Param('id') id: string) {
     return this.booksService.findOne(id);
   }
 
   @Put(':id')
+  @RateLimit({ points: 5, duration: 60 })
   update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
     return this.booksService.update(id, updateBookDto);
   }
 
   @Delete(':id')
+  @RateLimit({ points: 3, duration: 60 })
   remove(@Param('id') id: string) {
     return this.booksService.remove(id);
   }
